ff84fdb41d1ab4cdf93f2ce44bede7dfaab1c113
ShoveetSingh <shoveetsingh2002@gmail.com>
2023-07-27 18:14:43 +0530

    Second Commit

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,5 +1,7 @@
 import React,{useContext,createContext} from "react";
 import {useAddress,useContract,useMetamask,useContractWrite} from '@thirdweb-dev/react';
+import { id } from "ethers/lib/utils";
+import { ethers } from "ethers";
 //import {Mumbai} from 'Polygon';
 
 const StateContext = createContext();
@@ -28,11 +30,51 @@ export const StateContextProvider = ({children}) => {
     }
     }
 
+    const getCampaigns = async () => {
+        const campaigns = await contract.call('getCampaigns');
+
+        const parsedCampaigns = campaigns.map((campaign,id) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            image: campaign.image,
+            pId: id,
+                }))
+        return parsedCampaigns;
+    }
+
+    const getUserCampaigns = async () => {
+        const allCampaigns = await getCampaigns();
+        const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+        return filteredCampaigns;
+    }
+
+    const donate = async (pId,amount) => {
+        const data = await contract.call('donateToCampaign',pId,{value:ethers.utils.parseEther(amount)});
+        return data;
+    }
+
+    const getDonations = async (pId) => {
+        const donations = await contract.call('getDonators',pId);
+        const numberOfDonations = donations[0].length;
+        const parsedDonations = [];
+        for(let i=0;i<numberOfDonations;i++){
+         parsedDonations.push({
+            donator: donations[0][i],
+            donation:ethers.utils.formatEther(donations[1][i].toString())
+         })
+        }
+    }
+
     return(
-        <StateContext.Provider value={{address,connect,contract,createCampaign: publishCampaign,}}>
+        <StateContext.Provider value={{address,connect,contract,createCampaign: publishCampaign,getCampaigns,getUserCampaigns,donate,getDonations,}}>
             {children}
         </StateContext.Provider>
     );
 
 }
 export const  useStateContext = () => useContext(StateContext);
+  
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {useStateContext} from '../context'
+import {DisplayCampaigns} from '../components'
 
 const Profile = () => {
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [campaigns, setCampaigns] = useState([])
+  const {address, contract, getUserCampaigns} = useStateContext()
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if(contract){
+      fetchCampaigns();
+    }
+  },[contract]);
+
+  useEffect(() => {
+    if(contract){
+      fetchCampaigns();
+    }
+  },[address]);
+
+
   return (
-    <div>Profile</div>
+    <DisplayCampaigns title="ALL Campaigns" campaigns={campaigns} isLoading={isLoading} />
   )
 }
 
